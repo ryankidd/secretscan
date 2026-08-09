0dd47d0fd53df6ead3c9575b4ed32de24e885680
Ryan Kidd <itsryankidd@gmail.com>
2026-08-09 10:26:44 -0700

    Wire git history scanning into the CLI

--- main.go
+++ main.go
@@ -24,28 +24,33 @@ func (i *ignoreList) Set(v string) error {
 
 func main() {
 	var ignore ignoreList
+	var history bool
 	flag.Var(&ignore, "ignore", "glob pattern to skip when scanning a directory (repeatable); suffix with / to match directories only")
+	flag.BoolVar(&history, "history", false, "scan git commit history instead of the working tree; the argument is a path to a git repository")
 	flag.Parse()
 	args := flag.Args()
 	if len(args) != 1 {
-		fmt.Fprintln(os.Stderr, "usage: secretscan [-ignore pattern] <file|directory>")
+		fmt.Fprintln(os.Stderr, "usage: secretscan [-ignore pattern] [-history] <file|directory>")
 		os.Exit(2)
 	}
 
 	opts := scan.DefaultOptions()
 	opts.Ignore = ignore
 
-	info, err := os.Stat(args[0])
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "secretscan:", err)
-		os.Exit(1)
-	}
-
 	var findings []scan.Finding
-	if info.IsDir() {
-		findings, err = scan.Dir(args[0], opts)
+	var err error
+	if history {
+		findings, err = scan.GitHistory(args[0], opts)
 	} else {
-		findings, err = scan.File(args[0], opts)
+		var info os.FileInfo
+		info, err = os.Stat(args[0])
+		if err == nil {
+			if info.IsDir() {
+				findings, err = scan.Dir(args[0], opts)
+			} else {
+				findings, err = scan.File(args[0], opts)
+			}
+		}
 	}
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "secretscan:", err)
@@ -53,10 +58,15 @@ func main() {
 	}
 
 	for _, f := range findings {
+		loc := fmt.Sprintf("%s:%d", f.Path, f.Line)
+		if f.Commit != "" {
+			loc = fmt.Sprintf("%s@%s:%d", f.Path, f.Commit[:7], f.Line)
+		}
+
 		if f.Detector == "entropy" {
-			fmt.Printf("%s:%d: possible secret (entropy %.2f): %s\n", f.Path, f.Line, f.Entropy, f.Token)
+			fmt.Printf("%s: possible secret (entropy %.2f): %s\n", loc, f.Entropy, f.Token)
 		} else {
-			fmt.Printf("%s:%d: possible secret (%s): %s\n", f.Path, f.Line, f.Detector, f.Token)
+			fmt.Printf("%s: possible secret (%s): %s\n", loc, f.Detector, f.Token)
 		}
 	}
 
